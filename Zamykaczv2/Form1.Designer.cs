30040ccb312bfd25a45dde0d87a1b90c7e4cb632
pardon14 <pardon141414@gmail.com>
2023-03-21 18:53:50 +0100

    whole program

---
+++ Zamykaczv2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Zamykaczv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnShutdown = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShutdown
+            // 
+            this.btnShutdown.Location = new System.Drawing.Point(66, 234);
+            this.btnShutdown.Name = "btnShutdown";
+            this.btnShutdown.Size = new System.Drawing.Size(75, 38);
+            this.btnShutdown.TabIndex = 1;
+            this.btnShutdown.Text = "Shutdown system";
+            this.btnShutdown.UseVisualStyleBackColor = true;
+            this.btnShutdown.Click += new System.EventHandler(this.btnShutdown_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(66, 278);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 41);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel shutdown";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(66, 362);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 35);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Close application";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Shutdowner.Properties.Resources.clock;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd MMMM yyyy HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 198);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(189, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(213, 414);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnShutdown);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Shutdowner";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnShutdown;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Zamykaczv2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+
+namespace Zamykaczv2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        public static class ShutdownManager
+        {
+            public static void ScheduleShutdown(int seconds)
+            {
+                Process.Start("shutdown", $"/s /t {seconds}");
+            }
+
+            public static void CancelShutdown()
+            {
+                Process.Start("shutdown", "/a");
+            }
+        }
+
+        private void btnShutdown_Click(object sender, EventArgs e)
+        {
+
+            DateTime shutdownTime = dateTimePicker1.Value;
+
+            // Ustaw czas wyłączenia systemu
+            int shutdownTimeout = (int)(shutdownTime - DateTime.Now).TotalSeconds;
+            ShutdownManager.ScheduleShutdown(shutdownTimeout);
+
+            // Wyświetl powiadomienie o czasie wyłączenia systemu
+            TimeSpan ts = TimeSpan.FromSeconds(shutdownTimeout);
+            string timeToShutdown = string.Format("{0:D2}h:{1:D2}m:{2:D2}s",
+                                    ts.Hours, ts.Minutes, ts.Seconds);
+            MessageBox.Show($"The system will be shutdown in {timeToShutdown}", "System shutdown", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Anuluj wyłączenie systemu
+            ShutdownManager.CancelShutdown();
+
+            // Wyświetl powiadomienie o anulowaniu wyłączenia systemu
+            MessageBox.Show("The system shutdown has been canceled.", "Canceling system shutdown", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnExit_Click_1(object sender, EventArgs e)
+        {
+            // Zamknij aplikację
+            Application.Exit();
+
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "dd MMMM yyyy HH:mm:ss";
+            dateTimePicker1.ShowUpDown = true;
+        }
+    }
+}
